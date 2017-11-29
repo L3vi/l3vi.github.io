b2c212cef96838ea18a61ce4cc89c16afde5cdae
L3vi <levistum@gmail.com>
2017-11-29 01:57:26 -0700

    added some new files 'n stuff

---
+++ weather/javascript/search.js
@@ -0,0 +1,57 @@
+$('#query').keyup(function () {
+    let value = $(this).val();
+    let srch_value = new RegExp(value, "i");
+    $.getJSON("http://autocomplete.wunderground.com/aq?query=" + value + "&cb=?", function (data) {
+        console.log(data);
+        let output = '<ol>';
+        $.each(data.RESULTS, function (key, val) {
+            if (val.name.search(srch_value) != -1) {
+                output += '<li>';
+                output += '<a href="//www.wunderground.com' + val.l + '" title="See results for ' + val.name + '">' + val.name + '</a>';
+                output += '</li>';
+            }
+        });
+        output += '</ol>';
+
+        $("#searchResults").html(output);
+    });
+
+});
+
+// Get weather data from wunderground.com
+function getData(input) {
+    // Get the data from the wunderground API
+    $.ajax({
+        url: "//api.wunderground.com/api/436f9d65036b8775/geolookup/conditions/q/" +
+            input + ".json",
+        dataType: "jsonp",
+        success: function (data) {
+            console.log(data);
+            var location = data.location.city + ', ' + data.location.state;
+            var temp_f = data.current_observation.temp_f;
+            console.log('Location is: ' + location);
+            console.log('Temp is: ' + temp_f);
+            $("#cityDisplay").text(location);
+            $("title").html(location + " | Weather Center");
+            $("#currentTemp").html(Math.round(temp_f) + '°');
+            $("#summary").text(toTitleCase(data.current_observation.icon));
+            $("#cover").fadeOut(250);
+        }
+    });
+}
+
+// A function for changing a string to TitleCase
+function toTitleCase(str) {
+    return str.replace(/\w+/g, function (txt) {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+}
+
+// Intercept the menu link clicks
+$("#searchResults").on("click", "a", function (evt) {
+    evt.preventDefault();
+    // With the text value get the needed value from the weather.json file
+    var jsonCity = $(this).text(); // Franklin, etc...
+    console.log(jsonCity);
+    getData(jsonCity);
+});
