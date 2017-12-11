80400e0d8b9614f0957ec5a089b2fa05723bcdd7
L3vi <levistum@gmail.com>
2017-12-11 10:35:02 -0700

    Fixed things, hopefully for the last time

--- acme/js/script.js
+++ acme/js/script.js
@@ -1,6 +1,6 @@
 $(function (data) {
     let navBar = $("nav ul");
-    $.getJSON("/js/acme.json", function (data) {
+    $.getJSON("/acme/js/acme.json", function (data) {
         console.log(data);
         $.each(data, function (key, value) {
             $(navBar).append("<li>" + key + "</li>");
