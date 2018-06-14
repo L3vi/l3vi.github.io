222bcfabc7866d415c4a580bc2422ae3628de0a1
L3vi <levistum@gmail.com>
2018-06-14 10:33:15 -0600

    Updated timeSheet display

--- cit261/clockit/script.js
+++ cit261/clockit/script.js
@@ -3,7 +3,6 @@ var date1 = Date();
 var date2 = Date();
 var current = Date();
 const timeSheet = [];
-var time = 0;
 
 
 class Time {
@@ -11,16 +10,22 @@ class Time {
     constructor(startDate, endDate) {
         this.startDate = startDate;
         this.endDate = endDate;
-        this.milliseconds = parseInt(endDate - startDate) % 1000;
-        this.seconds = parseInt((endDate - startDate) / 1000) % 60;
-        this.minutes = parseInt(((endDate - startDate) / 1000) / 60) % 60;
-        this.hours = parseInt((((endDate - startDate) / 1000) / 60) / 60) % 60;
+        this.interval = (endDate - startDate);
+        //        this.interval = 45296000;
+        this.milliseconds = (this.interval) % 1000;
+        this.seconds = (this.interval / 1000) % 60;
+        this.minutes = ((this.interval / 1000) / 60) % 60;
+        this.hours = (((this.interval / 1000) / 60) / 60) % 60;
     }
 
     display() {
-        console.log(((this.hours < 10) ? "0" + this.hours : this.hours) + ":" + ((this.minutes < 10) ? "0" + this.minutes : this.minutes) + ":" + ((this.seconds < 10) ? "0" + this.seconds : this.seconds));
-        
-        return (((this.hours < 10) ? "0" + this.hours : this.hours) + ":" + ((this.minutes < 10) ? "0" + this.minutes : this.minutes) + ":" + ((this.seconds < 10) ? "0" + this.seconds : this.seconds));
+        console.log(((this.hours < 10) ? "0" + parseInt(this.hours) : parseInt(this.hours)) + ":" +
+            ((this.minutes < 10) ? "0" + parseInt(this.minutes) : parseInt(this.minutes)) + ":" +
+            ((this.seconds < 10) ? "0" + parseInt(this.seconds) : parseInt(this.seconds)));
+
+        return (((this.hours < 10) ? "0" + parseInt(this.hours) : parseInt(this.hours)) + ":" +
+            ((this.minutes < 10) ? "0" + parseInt(this.minutes) : parseInt(this.minutes)) + ":" +
+            ((this.seconds < 10) ? "0" + parseInt(this.seconds) : parseInt(this.seconds)));
     }
 
 }
@@ -40,26 +45,44 @@ function toggleClock() {
         let t1 = new Time(date1, date2);
 
         timeSheet.push(t1);
-        updateTimeSheet();
+        updateTimeSheet(t1);
         document.getElementById('toggle').innerHTML = "Clock In";
         document.getElementById('toggle').style.backgroundColor = "green";
     }
 }
 
 // Calls the timeClocked function every second to update the web page's timer.
-var update = setInterval(timeClocked, 100);
+var update = setInterval(displayTimeClockedIn, 100);
 
-function timeClocked() {
+function displayTimeClockedIn() {
     current = new Date();
     var currentTime = new Time(date1, current);
+
     if (isPressed == true) {
         document.getElementById('time').innerHTML = currentTime.display();
     }
 }
 
-function updateTimeSheet() {
-    document.getElementById('timeList').innerHTML = "";
-    for (let entry of timeSheet) {
-        document.getElementById('timeList').innerHTML += "<li>" + entry.display() + "</li>";
+function updateTimeSheet(entry) {
+
+    var weeklyTime = 0;
+    let timeTable = document.getElementById('timeList');
+
+    var row = timeTable.insertRow(timeSheet.indexOf(entry));
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+
+    cell1.innerHTML = entry.startDate.toDateString();
+//    Decides whether to display seconds, minutes, or hours
+    if (entry.minutes < 1) {
+        cell2.innerHTML = ((Math.floor(entry.seconds) == 1) ? ((Math.floor(entry.seconds)) + " Second") : ((Math.floor(entry.seconds)) + " Seconds"));
+    } else if (entry.hours < 1) {
+        cell2.innerHTML = ((entry.minutes > 2) ? ((Math.floor(entry.minutes)) + " Minutes") : ((Math.floor(entry.minutes)) + " Minute"));
+    } else {
+        cell2.innerHTML = (Math.floor(entry.hours * 100) / 100) + " Hours";
     }
+    
+
+    weeklyTime += Math.round(entry.hours * 10) / 10
+    document.getElementById('weeklyTime').innerHTML = weeklyTime;
 }
