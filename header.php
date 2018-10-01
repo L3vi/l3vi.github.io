fab466073fd767703c598c8260b7c4a2718e6d94
L3vi <levistum@gmail.com>
2018-10-01 15:32:48 -0600

    In the process of updatinng the header

---
+++ header.php
@@ -0,0 +1,14 @@
+<ul class="nav nav-pills">
+    <li class="nav-item">
+        <a class="nav-link active" href=cit230/index.html>CIT 230</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href=cit261/index.html>CIT 261</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#CS313">CS 313</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="https://l3vi.github.io/ePortfolio/">ePortfolio</a>
+    </li>
+</ul>
