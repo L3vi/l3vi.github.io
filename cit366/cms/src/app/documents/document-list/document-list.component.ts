c966585863308ffbc6bf64f25c4edabecff0481a
L3vi <levistum@gmail.com>
2019-05-16 21:57:19 -0600

    Added documents functionality

--- cit366/cms/src/app/documents/document-list/document-list.component.ts
+++ cit366/cms/src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
@@ -6,6 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent implements OnInit {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>()
+  documents: Document[] = [
+    new Document(1, "Resume", "List of skills n' stuff", "https://l3vi.github.io/ePortfolio/", []),
+    new Document(2, "CIT 230", "CIT 230 projects", "https://l3vi.github.io/cit230/index.html", []),
+    new Document(3, "CIT 261", "CIT 261 stuff", "https://l3vi.github.io/cit261/index.html", []),
+    new Document(4, "CIT 366", "Nothing for now", "https://l3vi.github.io/cit366/index.html", [])
+  ];
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
+  };
 
   constructor() { }
 
--- cit366/cms/src/app/documents/document.model.ts
+++ cit366/cms/src/app/documents/document.model.ts
@@ -1,9 +1,15 @@
 export class Document {
+    public id: Number;
+    public name: String;
+    public description: String;
+    public url: String;
+    public children: Document[];
+
     constructor(id: Number, name: String, description: String, url: String, children: Document[]) {
-        id = id;
-        name = name;
-        description = description;
-        url = url;
-        children = children;
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.url = url;
+        this.children = children;
     }
 }
